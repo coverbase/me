d4df8e730b3c0d9ccb4f3c91e3255b5dbeded4b5
Moritz <moritz.mueller@coverbase.co>
2023-11-14 16:59:30 +0100

    feat: Add projects to index page

--- pages/index.vue
+++ pages/index.vue
@@ -21,9 +21,39 @@
             </figcaption>
         </figure>
     </section>
+
+    <UDivider />
+
+    <UHeading class="text-center"> Projekte </UHeading>
+
+    <section class="grid sm:grid-cols-2 gap-4">
+        <div
+            class="flex items-start gap-4 bg-white shadow-sm p-4 rounded"
+            v-for="project in projects"
+        >
+            <div class="p-4 bg-slate-100 rounded">
+                <component :is="project.icon" />
+            </div>
+
+            <div class="flex flex-col">
+                <ULink
+                    target="_blank"
+                    class="font-semibold text-lg"
+                    :to="project.route"
+                    :as="NuxtLink"
+                >
+                    {{ project.title }}
+                </ULink>
+
+                <UParagraph> {{ project.description }} </UParagraph>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
+import { NuxtLink } from "#components";
+import { IconFileTypeSvg, IconPalette } from "@tabler/icons-vue";
 import csharp from "~/assets/csharp.svg";
 import nodejs from "~/assets/nodejs.svg";
 import nuxt from "~/assets/nuxt.svg";
@@ -50,6 +80,21 @@ const skills = [
     {
         path: nodejs,
         alt: "Nodejs",
+    },
+];
+
+const projects = [
+    {
+        icon: IconFileTypeSvg,
+        route: "https://coverbase.co",
+        title: "coverbase.co",
+        description: "Scalable Vector Graphics Editor",
+    },
+    {
+        icon: IconPalette,
+        route: "https://github.com/coverbase/ui",
+        title: "UI",
+        description: "Komponenten für Nuxt",
     },
 ];
 </script>
